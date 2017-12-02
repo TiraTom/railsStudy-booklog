dc568095a152a6be733ecf5b4e6fbf78572e2833
Tira <217bssr@gmail.com>
2017-12-02 03:56:12 +0000

    09_実習の実施

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129130625) do
+ActiveRecord::Schema.define(version: 20171202015114) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id"
@@ -31,6 +31,28 @@ ActiveRecord::Schema.define(version: 20171129130625) do
     t.string   "image"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "bools", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.string   "author"
+    t.string   "publisher"
+    t.integer  "price"
+    t.datetime "publish_date"
+    t.text     "caption"
+    t.string   "image"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.integer  "rank"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
