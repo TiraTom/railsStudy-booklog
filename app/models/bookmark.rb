24a1cdc6b0b7020211f039b2bf6d640d75fc06ac
Tira <217bssr@gmail.com>
2017-12-02 01:48:53 +0000

    08_実習の実施

---
+++ app/models/bookmark.rb
@@ -0,0 +1,10 @@
+class Bookmark < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :book
+  
+  validates :user_id, presence: true
+  validates :book_id, presence: true
+  
+  validates :user_id, :uniqueness => {:scope => :book_id}
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
+  resources :bookmarks, only: [:create, :destroy]
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
