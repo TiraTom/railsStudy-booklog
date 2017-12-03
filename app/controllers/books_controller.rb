5475b8e86aa794af1364b1dc7368021a56a1b65a
Tira <217bssr@gmail.com>
2017-12-03 01:33:21 +0000

    12_基本課題の実施

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,27 @@ class BooksController < ApplicationController
   
   def index
     if user_signed_in? && params[:ft] && params[:ft] == 'my'
-      @books = Book.includes(:bookmarks, :reviews, :user).where(user_id: current_user.id).order('updated_at DESC')
+      if params[:category_ft].present? 
+        @books = Book.includes(:user).where("user_id = ? AND category_id = ?", current_user.id, params[:category_ft]).order('updated_at DESC')
+      else
+        @books = Book.includes(:bookmarks, :reviews, :user).where("user_id = ? ", current_user.id).order('updated_at DESC')
+      end
     elsif user_signed_in? && params[:ft] && params[:ft] == 'bookmark'
-      @books = Book.joins(:bookmarks).where('bookmarks.user_id = ?', current_user.id).order('updated_at DESC')
+      if params[:category_ft].present? 
+        @books = Book.joins(:bookmarks).where("bookmarks.user_id = ? AND category_id = ?", current_user.id, params[:category_ft]).order('updated_at DESC')
+      else
+        @books = Book.joins(:bookmarks).where('bookmarks.user_id = ?', current_user.id).order('updated_at DESC')
+      end
     else
-      @books = Book.includes(:bookmarks, :reviews, :user).order('updated_at DESC')
+      if params[:category_ft].present? 
+        @books = Book.where('category_id = ?', params[:category_ft]).order('updated_at DESC')
+      else
+        @books = Book.includes(:bookmarks, :reviews, :user).order('updated_at DESC')
+      end
     end
+    
     @categories = Category.all
+
   end
   
   def show 
